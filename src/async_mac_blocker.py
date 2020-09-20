7332131c1eda27c5459de24694b2bb94d6ec6cbb
Ricky Laney <rlaneyjr@gmail.com>
2020-09-20 14:06:26 -0400

    Completed async_mac_blocker.py

--- src/async_mac_blocker.py
+++ src/async_mac_blocker.py
@@ -8,10 +8,12 @@ Description:        Find and block unauthorized clients from FHI-360 Meraki netw
 Author:             Ricky Laney
 Version:            0.0.2
 '''
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import csv
 from datetime import datetime
 import os
-import asyncio
+from manuf.manuf import MacParser
 import meraki.aio
 from typing import Union
 # Typing shortcuts
@@ -21,98 +23,168 @@ AIO = meraki.aio.AsyncDashboardAPI
 # Either input your API key below, or set an environment variable
 # for example, in Terminal on macOS:  export MERAKI_DASHBOARD_API_KEY=093b24e85df15a3e66f1fc359f4c48493eaa1b73
 YOUR_API_KEY = 'YOUR_API_KEY_GOES_HERE_DO_NOT_USE_THIS_ONE'
-api_key = os.environ.get('MERAKI_DASHBOARD_API_KEY', YOUR_API_KEY)
+API_KEY = os.environ.get('MERAKI_DASHBOARD_API_KEY', YOUR_API_KEY)
 
 # Changing this will change the policy for bad clients to "Blocked"
 BLOCK_BAD_CLIENTS = False
 
 HERE = os.path.dirname(os.path.abspath(__file__))
-base_url = 'https://api.meraki.com/api/v1'
+TODAY = f"{datetime.now():%m-%d-%Y}"
+LOG_DIR = os.path.join(HERE, 'logs')
+LOG_FILE_PREFIX = f"fhi-360_{TODAY}_"
+BASE_URL = 'https://api.meraki.com/api/v1'
+MAC_FILE = os.path.join(HERE, 'bad_macs.txt')
+COM_FILE = os.path.join(HERE, 'bad_companies.txt')
+
+CLIENT_FIELD_NAMES = [
+    'id',
+	'mac',
+	'description',
+	'ip',
+	'ip6',
+	'ip6Local',
+	'user',
+	'firstSeen',
+	'lastSeen',
+	'manufacturer',
+	'os',
+	'recentDeviceSerial',
+	'recentDeviceName',
+	'recentDeviceMac',
+	'ssid',
+	'vlan',
+	'switchport',
+	'usage',
+	'status',
+	'notes',
+	'smInstalled',
+	'groupPolicy8021x',
+]
+
+# Load mac_parser globally as it takes a while
+try:
+    mac_parser = MacParser(update=True)
+except Exception as e:
+    print(f"Unable to update manuf database due to {e}")
+    mac_parser = MacParser()
 
 
 class FHI360ClientError(Exception):
     """ Custom error class
     """
 
-class ClientValidator:
-    """ Class that validates clients on the network
 
-    :param: use_manuf = True (default)
-    :return: bool = represents valid (True) and invalid (False) clients
-    """
+def get_bad_macs(mac_file=MAC_FILE) -> Union[list, None]:
+    with open(mac_file) as mf:
+        bad_macs = mf.read().splitlines()
+    return bad_macs or None
 
-    def __init__(self, use_manuf: bool=True) -> None:
-        self._use_manuf = use_manuf
-        self._mac_file = os.path.join(HERE, 'bad_macs.txt')
-        self._com_file = os.path.join(HERE, 'bad_companies.txt')
 
-        if self._use_manuf:
-            from manuf.manuf import MacParser
-            try:
-                mac_parser = MacParser(update=True)
-            except Exception as e:
-                print(f"Unable to update manuf database due to {e}")
-                mac_parser = MacParser()
-            self.parser = mac_parser
-        # Load these from file once and use throughout
-        self.bad_macs = self._get_bad_macs()
-        self.bad_coms = self._get_bad_companies()
+def get_bad_companies(com_file=COM_FILE) -> Union[list, None]:
+    with open(com_file) as cf:
+        bad_coms = cf.read().splitlines()
+    return bad_coms or None
 
-    def _get_bad_macs(self) -> list:
-        with open(self._mac_file) as mf:
-            bad_macs = mf.read().splitlines()
-        return bad_macs or None
 
-    def _get_bad_companies(self) -> list:
-        with open(self._com_file) as cf:
-            bad_coms = cf.read().splitlines()
-        return bad_coms or None
+async def is_bad_client(client: dict, bad_macs: Union[dict, None],
+                  bad_coms: Union[list, None] ) -> dict:
+    company = client['manufacturer'] or None
+    mac = client['mac'] or None
+    manuf_company = mac_parser.get_manuf(mac)
+    if bad_coms and any([company, manuf_company]):
+        for bad_com in bad_coms:
+            if company and bad_com in company:
+                return client
+            if manuf_company and bad_com in manuf_company:
+                return client
+    if bad_macs and mac:
+        for bad_mac in bad_macs:
+            if mac.startswith(bad_mac):
+                return client
 
-    def is_bad_company(self, company: str) -> bool:
-        if self.bad_coms and company:
-            for bad_com in self.bad_coms:
-                if bad_com in company:
-                    return True
-        return False
 
-    def is_bad_mac(self, mac: str) -> bool:
-        if self.bad_macs:
-            for bad_mac in self.bad_macs:
-                if mac.startswith(bad_mac):
-                    return True
-        if self._use_manuf:
-            mac_com = self.parser.get_manuf(mac)
-            return self.is_bad_company(mac_com)
-        return False
-
-    def is_bad_client(self, client: dict) -> bool:
-        if self.is_bad_mac(client['mac']):
-            return True
-        if self.is_bad_company(client['manufacturer']):
-            return True
-        return False
-
-# Initialize a global ClientValidator
-CV = ClientValidator()
-
-def clean_usage(clients: list) -> list:
-    # Reformat usage for readability
-    for client in clients:
+async def clean_client(client: dict) -> dict:
+    # Flatten and reformat usage for readability
+    if client['usage']:
         sent_usage = client['usage']['sent']
         recv_usage = client['usage']['recv']
         client['usage'] = f"sent={sent_usage} recv={recv_usage}"
-        client['blocked'] = False
-    return clients
+    client['blocked'] = False
+    return client
 
-async def block_client(meraki_aio: AIO, network: dict, client: dict):
+
+async def get_bad_clients(clients: list, clean: bool=True) -> Union[list, None]:
+    """ Function that runs ourasync and non-async functions for determining bad
+        clients.
+    """
+    num_workers = os.cpu_count() * 4
+    executor = ThreadPoolExecutor(max_workers=num_workers)
+    loop = asyncio.get_event_loop()
+    bad_macs = await loop.run_in_executor(executor, get_bad_macs)
+    bad_coms = await loop.run_in_executor(executor, get_bad_companies)
+    bad_clients = []
+    client_tasks = [is_bad_client(client, bad_macs, bad_coms) for client in clients]
+    for task in asyncio.as_completed(client_tasks):
+        bad_client = await task
+        if bad_client:
+            if clean:
+                bad_client = await clean_client(bad_client)
+            bad_clients.append(bad_client)
+    return bad_clients
+
+# bad_clients = [yield from await is_bad_client(client, bad_macs, bad_coms) for client in clients]
+
+
+async def block_client(meraki_aio: AIO, network: dict, client: dict) -> str:
     resp = await meraki_api.networks.updateNetworkClientPolicy(
         network['id'],
         client['id'],
         'Blocked',
     )
     if resp and resp['devicePolicy'] == 'Blocked':
-        return client, True
-    return client, False
+        client['blocked'] = True
+        return f"SUCCESS blocking: {client['id']}"
+    return f"FAILED blocking: {client['id']}"
+
+
+async def write_final_report(folder_name: str, nets: list) -> str:
+    # Stitch together one consolidated CSV for this org
+    final_report = os.path.join(HERE, f"{folder_name}.csv")
+    output_file = open(final_report, mode="w", newline="\n")
+    field_names = CLIENT_FIELD_NAMES
+    field_names.insert(0, 'Network Name')
+    field_names.insert(1, 'Network ID')
+    field_names.append('blocked')
+    csv_writer = csv.DictWriter(
+        output_file,
+        field_names,
+        delimiter=',',
+        quotechar='"',
+        quoting=csv.QUOTE_ALL,
+    )
+    csv_writer.writeheader()
+    for net in nets:
+        file_name = f"{net['name'].replace(' ', '')}.csv"
+        dir_name = os.path.join(HERE, folder_name)
+        if file_name in os.listdir(dir_name):
+            with open(os.path.join(dir_name, file_name)) as input_file:
+                csv_reader = csv.DictReader(
+                    input_file,
+                    delimiter=',',
+                    quotechar='"',
+                    quoting=csv.QUOTE_ALL,
+                )
+                try:
+                    next(csv_reader)
+                except StopIteration:
+                    continue
+                else:
+                    for row in csv_reader:
+                        row['Network Name'] = net['name']
+                        row['Network ID'] = net['id']
+                        csv_writer.writerow(row)
+    return f"Final report written to {final_report}"
+
 
 async def get_clients(meraki_aio: AIO, network: dict, dir_name: str) -> str:
     print(f"Searching clients in network {network['name']}")
@@ -129,44 +201,41 @@ async def get_clients(meraki_aio: AIO, network: dict, dir_name: str) -> str:
         print(f"ERROR: get_clients: Unknown error: {e}")
     else:
         if clients:
-            bad_clients = [client for client in clients if \
-                           CV.is_bad_client(client)]
+            print(f"Found {len(clients)} clients in {network['name']}")
+            bad_clients = await get_bad_clients(clients)
             if bad_clients:
-                clients = clean_usage(bad_clients)
-                print(f"Found {len(clients)} bad clients total")
+                print(f"Found {len(bad_clients)} bad clients")
                 if BLOCK_BAD_CLIENTS:
                     # Create a list to call concurrently
                     block_tasks = [block_client(
                         meraki_aio,
                         network,
                         client
-                    ) for client in clients]
+                    ) for client in bad_clients]
                     for task in asyncio.as_completed(block_tasks):
-                        client, success = await task
-                        if success:
-                            client['blocked'] = True
-                            print(f"Successfully blocked: {client['id']}")
-                        else:
-                            print(f"FAILED to block: {client['id']}")
+                        result = await task
+                        print(result)
                 file_name = f"{network['name'].replace(' ', '')}.csv"
                 output_file = open(f"{dir_name}/{file_name}", mode='w',
                                     newline='\n')
-                field_names = clients[0].keys()
+                field_names = bad_clients[0].keys()
                 csv_writer = csv.DictWriter(output_file, field_names,
                                             delimiter=',', quotechar='"',
                                             quoting=csv.QUOTE_ALL)
                 csv_writer.writeheader()
-                csv_writer.writerows(clients)
+                csv_writer.writerows(bad_clients)
                 output_file.close()
-                print(f"Successfully output {len(clients)} clients' data to \
+                print(f"Successfully output {len(bad_clients)} clients' data to \
                       file {file_name}")
             else:
                 print(f"No bad clients found in {network['name']}")
     return network['name']
 
+
 async def get_networks(meraki_aio: AIO, org: dict) -> str:
     org_id = org['id']
     org_name = org['name']
+    nets = None
     print(f"\nAnalyzing organization {org_name}:")
     try:
         nets = await meraki_aio.organizations.getOrganizationNetworks(
@@ -175,13 +244,11 @@ async def get_networks(meraki_aio: AIO, org: dict) -> str:
         )
     except meraki.aio.AsyncAPIError as e:
         print(f"ERROR: get_networks: Meraki API error: {e}")
-        raise e
     except Exception as e:
         print(f"ERROR: get_networks: Unknown error: {e}")
-        raise e
-
-    tday = f"{datetime.now():%m-%d-%Y}"
-    folder_name = f"{org_name}_clients_{tday}"
+    if not nets:
+        raise FHI360ClientError(f"Failed to get networks from {org_name}")
+    folder_name = f"{org_name}_clients_{TODAY}"
     dir_name = os.path.join(HERE, folder_name)
     if folder_name not in os.listdir(HERE):
         os.mkdir(dir_name)
@@ -193,65 +260,30 @@ async def get_networks(meraki_aio: AIO, org: dict) -> str:
     for task in asyncio.as_completed(get_clients_tasks):
         net_name = await task
         print(f"Finished network: {net_name}")
-
-    # Stitch together one consolidated CSV for this org
-    output_file = open(f"{folder_name}.csv", mode="w", newline="\n")
-    field_names = ['id', 'mac', 'description', 'ip', 'ip6', 'ip6Local', 'user', 'firstSeen', 'lastSeen', 'manufacturer', 'os', 'recentDeviceSerial', 'recentDeviceName', 'recentDeviceMac', 'ssid', 'vlan', 'switchport', 'usage', 'status', 'notes', 'smInstalled', 'groupPolicy8021x']
-    field_names.insert(0, 'Network Name')
-    field_names.insert(1, 'Network ID')
-    field_names.append('blocked')
-    csv_writer = csv.DictWriter(
-        output_file,
-        field_names,
-        delimiter=',',
-        quotechar='"',
-        quoting=csv.QUOTE_ALL,
-    )
-    csv_writer.writeheader()
-    for net in nets:
-        file_name = f"{net['name'].replace(' ', '')}.csv"
-        if file_name in os.listdir(dir_name):
-            with open(f"{dir_name}/{file_name}") as input_file:
-                csv_reader = csv.DictReader(
-                    input_file,
-                    delimiter=',',
-                    quotechar='"',
-                    quoting=csv.QUOTE_ALL,
-                )
-                try:
-                    next(csv_reader)
-                except StopIteration:
-                    continue
-                else:
-                    for row in csv_reader:
-                        row['Network Name'] = net['name']
-                        row['Network ID'] = net['id']
-                        csv_writer.writerow(row)
+    results = await write_final_report(folder_name, nets)
+    print(results)
     return org_name
 
 
 async def main():
     start_time = datetime.now()
-    tday = f"{datetime.now():%m-%d-%Y}"
-    log_file_prefix = f"fhi-360_{tday}_"
-    log_dir = os.path.join(HERE, "logs")
     if "logs" not in os.listdir(HERE):
-        os.mkdir(log_dir)
+        os.mkdir(LOG_DIR)
     # Instantiate a Meraki Async dashboard API session
     async with meraki.aio.AsyncDashboardAPI(
-            api_key=api_key,
-            base_url=base_url,
+            api_key=API_KEY,
+            base_url=BASE_URL,
             output_log=True,
-            log_file_prefix=log_file_prefix,
-            log_path=log_dir,
+            log_file_prefix=LOG_FILE_PREFIX,
+            log_path=LOG_DIR,
             print_console=False
         ) as meraki_aio:
-        fhi_id = '324893'
-        org = await meraki_aio.organizations.getOrganization(fhi_id)
+        fhi360 = '324893'
+        org = await meraki_aio.organizations.getOrganization(fhi360)
         org_name = await get_networks(meraki_aio, org)
     end_time = datetime.now()
-    print(f"Completed org: {org_name}")
-    print(f"\nScript complete, total runtime {end_time - start_time}")
+    print(f"Completed org: {org_name}\n")
+    print(f"Total runtime: {end_time - start_time}")
 
 
 if __name__ == "__main__":
